32a6ac7c944ef8a912a4ff60586ce43ffbf4da29
Laurentiu Stamate <laurentiu.stamate@equilobe.com>
2018-03-23 10:24:42 +0100

    Add filter and middleware used by AspNetCore

---
+++ Serilog.Enrichers.AspNetCore/HttpContextFilter.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Serilog.Enrichers.AspNetCore
+{
+    public class HttpContextFilter : IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var httpContextModel = context.HttpContext.RequestServices.GetService<HttpContextModel>();
+
+            httpContextModel.ControllerName = context.Controller.ToString();
+            httpContextModel.ActionName = context.ActionDescriptor.DisplayName;
+            httpContextModel.RouteData = context.RouteData.Values.ToDictionary(x => x.Key, y => y.Value);
+            httpContextModel.RequestId = httpContextModel.RequestId ?? context.HttpContext.TraceIdentifier;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // Nothing to do :-)
+        }
+    }
+}
---
+++ Serilog.Enrichers.AspNetCore/HttpContextMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace Serilog.Enrichers.AspNetCore
+{
+    public class HttpContextMiddleware
+    {
+        private readonly RequestDelegate _next = null;
+
+        public HttpContextMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public Task InvokeAsync(HttpContext context, HttpContextModel httpContextModel)
+        {
+            httpContextModel.UserClaims = context.User?.Claims;
+            httpContextModel.Host = context.Request.Host;
+            httpContextModel.Location = context.Request.Path;
+            httpContextModel.Headers = context.Request.Headers.ToDictionary(x => x.Key, y => y.Value);
+            httpContextModel.QueryString = QueryHelpers.ParseQuery(context.Request.QueryString.ToString()).ToDictionary(x => x.Key, y => y.Value);
+            httpContextModel.RequestId = context.TraceIdentifier ?? Guid.NewGuid().ToString("N").ToLowerInvariant();
+
+            // Call the next delegate/middleware in the pipeline
+            return this._next(context);
+        }
+    }
+}
