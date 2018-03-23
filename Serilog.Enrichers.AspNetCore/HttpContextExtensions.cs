83dc47bb6bee0bcf9442d8bdb83232a5c191e66a
Laurentiu Stamate <laurentiu.stamate@equilobe.com>
2018-03-23 10:25:32 +0100

    Add extensions and files to project

---
+++ Serilog.Enrichers.AspNetCore/HttpContextExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog.Configuration;
+
+namespace Serilog.Enrichers.AspNetCore
+{
+    public static class HttpContextExtensions
+    {
+        public static IApplicationBuilder UseHttpContextMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HttpContextMiddleware>();
+        }
+
+        public static IServiceCollection AddHttpContextMiddleware(this IServiceCollection services)
+        {
+            services.AddScoped<HttpContextModel>();
+            services.AddScoped<HttpContextEnricher>();
+
+            return services;
+        }
+
+        public static LoggerConfiguration WithHttpContext(this LoggerEnrichmentConfiguration loggerEnrichmentConfiguration, IServiceProvider services)
+        {
+            return loggerEnrichmentConfiguration.With(services.GetService<HttpContextEnricher>());
+        }
+    }
+}
