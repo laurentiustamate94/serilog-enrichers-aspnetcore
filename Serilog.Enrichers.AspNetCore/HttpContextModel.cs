48f7d27b596c52655950eb49045dbefd127b1f81
Laurentiu Stamate <laurentiu.stamate@equilobe.com>
2018-03-23 10:22:44 +0100

    Add context model used for enriching

---
+++ Serilog.Enrichers.AspNetCore/HttpContextModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace Serilog.Enrichers.AspNetCore
+{
+    public class HttpContextModel
+    {
+        public IEnumerable<Claim> UserClaims { get; internal set; }
+
+        public HostString Host { get; internal set; }
+
+        public string RequestId { get; internal set; }
+
+        public Dictionary<string, StringValues> Headers { get; internal set; }
+
+        public Dictionary<string, StringValues> QueryString { get; internal set; }
+
+        public PathString Location { get; internal set; }
+
+        public Dictionary<string, object> RouteData { get; internal set; }
+
+        public string ControllerName { get; internal set; }
+
+        public string ActionName { get; internal set; }
+    }
+}
