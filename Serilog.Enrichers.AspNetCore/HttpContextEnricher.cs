d56e88dcb1ab456e4a20a01425214be2456eaf06
Laurentiu Stamate <laurentiu.stamate@equilobe.com>
2018-03-23 10:23:00 +0100

    Add serilog httpcontext enricher

---
+++ Serilog.Enrichers.AspNetCore/HttpContextEnricher.cs
@@ -0,0 +1,25 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Enrichers.AspNetCore
+{
+    public class HttpContextEnricher : ILogEventEnricher
+    {
+        private readonly HttpContextModel _model = null;
+
+        public HttpContextEnricher(HttpContextModel model)
+        {
+            this._model = model;
+        }
+
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            if (this._model == null)
+            {
+                return;
+            }
+
+            logEvent.AddOrUpdateProperty(propertyFactory.CreateProperty("HttpContext", this._model, true));
+        }
+    }
+}
